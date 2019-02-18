37a4459a8493e88c3c26cb95eaa939488a836a17
dhibin <dhibink@gmail.com>
2019-02-18 23:31:19 +0530

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function () { //built-in event
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        from: 'dk',
-        text: 'Hey, how are you?'
-    });
+    // socket.emit('createMessage', {
+    //     from: 'dk',
+    //     text: 'Hey, how are you?'
+    // });
 });
 
 socket.on('disconnect', function () { //built-in event
